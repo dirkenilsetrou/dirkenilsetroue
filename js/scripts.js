7400635da32638d1d7267f98ca311c6ca267dff0
dirkenilsetrou <dirkenilsetrou@gmail.com>
2025-01-22 23:09:52 +0200

    update

--- js/scripts.js
+++ js/scripts.js
@@ -255,7 +255,7 @@ function initMap() {
 }
 
 function initBBSRMap() {
-    var la_fiesta = {lat: 20.305826, lng: 85.85480189999998};
+    var la_fiesta = {lat: -25.840815439396717, lng: 28.177404308483233};
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
         zoom: 15,
         center: la_fiesta,
